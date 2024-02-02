a48e9ee1385d63fe0e967e401b0f0b159fccd1e2
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-02-02 16:47:16 +0400

    Update bubblesort.java

--- bubblesort.java
+++ bubblesort.java
@@ -1,18 +1,23 @@
-public class bubblesort{
-	public static void main(String[] arg){
-		int min=0;
-		int [] numbers={3, 7, 1, 5};
-		for (int j=0; j<numbers.length; j++){
-			for (int i=1; i<numbers.length; i++){
-				if (numbers[i-1]>numbers[i]){
-					min=numbers[i];
-					numbers[i]=numbers[i-1];
-					numbers[i-1]=min;
-				}
-			}
-		}
-		for (int k:numbers){
-			System.out.println(k);
-		}
-	}
+public class bubblesort {
+  public static void main(String[] arg) {
+    int min = 0;
+    int[] numbers = {
+      3,
+      7,
+      1,
+      5
+    };
+    for (int j = 0; j < numbers.length; j++) {
+      for (int i = 1; i < numbers.length; i++) {
+        if (numbers[i - 1] > numbers[i]) {
+          min = numbers[i];
+          numbers[i] = numbers[i - 1];
+          numbers[i - 1] = min;
+        }
+      }
+    }
+    for (int k: numbers) {
+      System.out.println(k);
+    }
+  }
 }
